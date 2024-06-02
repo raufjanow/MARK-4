c2b2c5ce56e627e913fedc16d472d567188fb491
Mirafzal Raufjanov <michaeltolever@gmail.com>
2024-06-02 18:56:29 +0500

    First 5 th commit

---
+++ main.py
@@ -0,0 +1,35 @@
+                                         # 4 MONTH
+
+                                         # LS 34
+
+# Task 1
+# n = int(input('Write the num: '))
+# print(n+n*2)
+
+# Task 2
+# w = 46
+# st = '46'
+# print(w,st*5)
+
+# Task 3
+# fv = 5
+# f = 'F'
+# hi = 'Привет'
+# ni = 90.2
+# six = 67
+#
+# print(hi)
+
+# Task 4
+# num = str(input('Write the four-digit num: '))
+# num = list(str(num))
+# print(num)
+
+# Task5
+dic ={
+    'name' : input('Write the name: '),
+    'Surname': input('Write the surname: '),
+    'age' : input('Write the age: '),
+
+}
+print(dic)
