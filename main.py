807bfa3a974c63ed0d53ceb7bfd83ba2265bee06
Mirafzal Raufjanov <michaeltolever@gmail.com>
2024-06-02 20:15:26 +0500

    Third 5 th commit

--- main.py
+++ main.py
@@ -82,5 +82,54 @@
 #
 # passw(pas,sec_pas)
 
+# Task 11
+# class Calc:
+#
+#
+#     def add(self,a,b):
+#         return a + b
+#     def minus(self,a,b):
+#         return a - b
+#     def mult(self,a,b):
+#         return a * b
+#     def div(self,a,b):
+#         return a / b
+#
+# cal = Calc()
+# def input_ask():
+#     a = int(input('First Number: '))
+#     b = int(input('Second Number: '))
+#     return a,b
+# c = input("Choose between(+, -, *, /):  ")
+# if c in ['+','-','*','/']:
+#     num1,num2 = input_ask()
+#
+#
+#
+#     if c == '+':
+#         print('Result: ',cal.add(num1,num2))
+#     if c == '-':
+#         print('Result: ',cal.minus(num1,num2))
+#     if c == '*':
+#         print('Result: ',cal.mult(num1,num2))
+#     if c == '/':
+#         print('Result: ',cal.div(num1,num2))
 
+
+# Task 12
+# print('Hello World!')
+
+# Task 13
+# Переменные в Python представляют собой способ хранения данных,
+                                         # которые могут быть использованы и изменены в программе.
+
+# Task 14
+#Циклы позволяют выполнять некоторое действие в зависимости от соблюдения некоторого условия.
+                                         # В языке Python есть следующие типы циклов:
+                                                # while
+                                                # for
+# Task 15
+#Работа со списками в Python включает в себя создание, изменение, доступ к элементам
+                                         # и выполнение различных операций над ними.
+                                         # Вот основные методы работы со списками:
 
